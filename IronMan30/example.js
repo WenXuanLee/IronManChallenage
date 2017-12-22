f66ca4051bfa0671c313b2fe7d6ca3c5d1a83300
Wen Xuan Lee <speechless0922@gmail.com>
2017-12-22 12:30:52 +0800

    adjust

--- IronMan30/example.js
+++ IronMan30/example.js
@@ -2,3 +2,12 @@ function hello() {
 	var test = "hello world";
 }
 
+var a = "Hey";
+console.log(a);
+
+if (a === undefined) {
+	console.log('a is undefined');
+}else {
+	console.log('a is defined');
+}
+// a is undefined
