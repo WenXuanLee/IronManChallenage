cd55b668bf3aebef2573c51dbed1178bec06bcc5
Wen Xuan Lee <speechless0922@gmail.com>
2017-12-26 16:08:00 +0800

    Day24 done

--- IronMan30/example.js
+++ IronMan30/example.js
@@ -1,13 +1,29 @@
-var friends ={
-	name: 'Ben'
-	greet: function() {
-		return this.name;
+var person = {
+	firstName: 'Default',
+	lastName: 'Default',
+	getFullName: function() {
+		return this.firstName + ' ' + this.lastName;
+	}
+
+}
+
+var john = {
+	firstName: 'Ben',
+	lastName: 'Lee'
+}
+
+//千萬不要亂用__proto__，此為demo。
+john.__proto__ = person;
+
+for ( var prop in john) {
+	console.log(prop + ': ' + john[prop]);
+}
+
+var jane = {
+	addess: '111 Main St.',
+	getFormalFullName: function() {
+		return this.lastName + ', ' + this.firstName;
 	}
 }
 
-var myFriends = function(argu1, argu2) {
-
-	console.log('Hi' + this.greet());
-}
-
-myFriends();
+$.extend(true, john, jane);
